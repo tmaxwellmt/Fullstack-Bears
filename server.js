47bd4fb8af9d8e1cbe6c075089dbfac96ab3132f
tmaxwellmt <tmaxwellmt@gmail.com>
2017-01-30 11:38:00 -0700

    Happy commit Ignore

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 app.get ('/api/greeting', function(req,res){
   res.send("Happy Monday");
 });
-app.get(/api/happy',function(req,res) {
+app.get('/api/happy',function(req,res) {
   res.send("Happy");
 
 app.listen(3000, function(){
