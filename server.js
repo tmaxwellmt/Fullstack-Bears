0e0b95c0af0134a36a81eeb1b155f7843a12b581
tmaxwellmt <tmaxwellmt@gmail.com>
2017-01-30 11:44:59 -0700

    Happy commit Ignore

--- server.js
+++ server.js
@@ -4,8 +4,19 @@ var app = express();
 app.get ('/api/greeting', function(req,res){
   res.send("Happy Monday");
 });
+
+app.get('/api/greeting', function(req, res) {
+  console.log(req.params.name);
+  res.send("Happy Monday" + name);
+});
+
 app.get('/api/happy',function(req,res) {
   res.send("Happy");
+});
+
+app.get('/api/sad',function(req,res) {
+  res.send("So confused.");
+});
 
 app.listen(3000, function(){
   console.log("Express application fired up on port 3000")
