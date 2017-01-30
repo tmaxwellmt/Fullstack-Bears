24aac4fefed944aa104b9d61c79c7c04fc5e9281
tmaxwellmt <tmaxwellmt@gmail.com>
2017-01-30 10:56:45 -0700

    fullstack

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
-console.log("Hello, World");
+var http = require('http');
+http.createServer(function(req, res) {
+res.send("Hello World");
+}).listen(3000);
+
+console.log("Server is up and running")
